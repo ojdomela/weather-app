26555ee7289c255b4a74ffb89c6da8c2cc33d648
Jens <ojdomela@gmail.com>
2022-08-09 19:54:43 +0200

    front and back start

---
+++ src/components/cards/CardBack.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CardBack({ data }) {
+    console.log(data)
+  return (
+    <div>CardBack</div>
+  )
+}
---
+++ src/components/cards/CardFront.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CardFront({ data }) {
+    console.log(data)
+  return (
+    <div>CardFront</div>
+  )
+}
